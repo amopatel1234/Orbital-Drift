472055494c1701dbf9cc047019ceadbaf6ddb69f
Amish Patel <amopatel1234@gmail.com>
2025-08-24 18:30:15 +0800

    fix: bug when input stops when near miss happens (#5)

--- Soruces/View+Shake.swift
+++ Soruces/View+Shake.swift
@@ -1,8 +1,8 @@
 import SwiftUI
 
+// MARK: - (Optional) small helper if you kept the shake modifier separate
 extension View {
     func screenShake(_ amount: CGFloat) -> some View {
-        // Respect Reduce Motion
         if UIAccessibility.isReduceMotionEnabled || amount <= 0 { return AnyView(self) }
         let x = CGFloat.random(in: -amount...amount)
         let y = CGFloat.random(in: -amount...amount)
